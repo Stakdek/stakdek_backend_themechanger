6ad61e04c3f65c235d2b2727c6d2593c23e0fdaa
Dennis Wulfert <dennis.wulfert@perfact.de>
2021-04-20 09:44:19 +0200

    Add main files for odoo module

---
+++ __manifest__.py
@@ -0,0 +1,28 @@
+{
+    'name': 'Odoo Backend Theme changer',
+    'version': '14.0.1.0.1',
+    'summary': 'Odoo Backend Theme changer',
+    'author': 'STAKDEK',
+    'license': 'AGPL-3',
+    'maintainer': 'STAKDEK',
+    'company': 'STAKDEK',
+    'website': 'https://stakdek.de',
+    'depends': [
+        'web'
+    ],
+    'category':'Branding',
+    'description': """
+           Odoo Backend Theme changer
+    """,
+   'data': [
+
+    'views/webclient_template_extend.xml',
+
+    ],
+    'price':0,
+    'currency':'USD',
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+    'images': ['static/description/icon.png','static/description/main_screenshot.png']
+}
